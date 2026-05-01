9e0a8939ef69ce9a3a541a173659cfae601e4a68
moses8-tech <mosesloukae9@gmail.com>
2026-05-01 09:34:50 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:hushray05@gmail.com">mosesloukai20@gmail.com</a>
+         <a href="mailto:hushray05@gmail.com">loukaemoses@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>00:07am to 00:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Kampala, Uganda - 400104</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:+256784465531">+256784465531</a>
+         <a href="tel:+256791573975">+256791573975</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Moses Loukae</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
